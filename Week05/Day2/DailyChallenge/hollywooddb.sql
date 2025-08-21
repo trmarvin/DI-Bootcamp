43f7cff52b4bd00512c315a31d9d6eecb95113d8
Tamar Ron Marvin <trmarvin@gmail.com>
2025-08-21 21:05:23 +0300

    resubmitting W5D2 DC

--- Week05/Day2/DailyChallenge/hollywooddb.sql
+++ Week05/Day2/DailyChallenge/hollywooddb.sql
@@ -1,2 +1,26 @@
-INSERT INTO actors (first_name, last_name, birthdate, number_oscars)
-VALUES('Douglas', 'Kirk', 1);
+-- -- SELECT COUNT(*) AS total_actors
+-- -- FROM actors;
+
+-- -- SELECT * FROM actors
+
+-- -- INSERT INTO actors (first_name, last_name, birthdate, number_oscars)
+-- -- 	('Tom', 'Hanks', '1956-07-09');
+
+-- -- 	result: 
+-- -- ERROR:  syntax error at or near "'Tom'"
+-- -- LINE 7:  ('Tom', 'Hanks', '1956-07-09');
+-- --           ^ 
+
+-- -- SQL state: 42601
+-- -- Character: 149
+
+-- INSERT INTO actors (first_name, last_name, number_oscars)
+-- 	('Denzel', 'Washington', 2);
+
+-- result:
+-- ERROR:  syntax error at or near "'Denzel'"
+-- LINE 18:  ('Denzel', 'Washington', 2);
+--            ^ 
+
+-- SQL state: 42601
+-- Character: 403
