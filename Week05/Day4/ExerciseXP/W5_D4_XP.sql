24311e03dc7b54e22b4109372f3740e0acbf849d
Tamar Ron Marvin <trmarvin@gmail.com>
2025-08-24 14:06:32 +0300

    Updating W5D4 DC

--- Week05/Day4/ExerciseXP/W5_D4_XP.sql
+++ Week05/Day4/ExerciseXP/W5_D4_XP.sql
@@ -1,2 +1,23 @@
-SELECT last_name FROM customers
-ORDER BY last_name DESC
+ -- SELECT COUNT(*) 
+ --    FROM FirstTab AS ft 
+	-- WHERE ft.id NOT IN ( 
+	-- 	SELECT id FROM SecondTab WHERE id IS NULL 
+	-- 	)
+
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft 
+-- 	WHERE ft.id NOT IN ( 
+-- 		SELECT id FROM SecondTab WHERE id = 5 
+-- 		)
+
+-- SELECT COUNT(*) 
+--     FROM FirstTab AS ft 
+-- 	WHERE ft.id NOT IN ( 
+-- 		SELECT id FROM SecondTab 
+-- 		)
+
+SELECT COUNT(*) 
+    FROM FirstTab AS ft 
+	WHERE ft.id NOT IN ( 
+		SELECT id FROM SecondTab WHERE id IS NOT NULL 
+		)
