306509c1a74d820bd4ec4364c5234b3553932b98
Tamar Ron Marvin <trmarvin@gmail.com>
2025-09-04 14:41:15 +0300

    saving XP

--- Week07/Day4/ExercisesXP/W7_D4_XP.js
+++ Week07/Day4/ExercisesXP/W7_D4_XP.js
@@ -8,3 +8,14 @@ console.log(people.slice(1, 3));
 console.log(people.indexOf("Foo")); // this returns -1 because that's the default value for an item not in the array
 let last = people[people.length - 1];
 console.log(last);
+
+for (let i = 0; i < people.length; i++) {
+  console.log(people[i]);
+}
+
+for (let i = 0; i < people.length; i++) {
+  if (people[i] === "Devon") {
+    break;
+  }
+  console.log(people[i]);
+}
