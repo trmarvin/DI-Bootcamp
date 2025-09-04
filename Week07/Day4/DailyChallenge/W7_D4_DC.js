176c770f05bf6c02e3292c2e4efda215a0b33df9
Tamar Ron Marvin <trmarvin@gmail.com>
2025-09-04 15:50:33 +0300

    DC 1

---
+++ Week07/Day4/DailyChallenge/W7_D4_DC.js
@@ -0,0 +1,19 @@
+let sentence = "Broccoli is not that bad, I like it"
+let wordNot = sentence.indexOf("not");
+let wordBad = sentence.indexOf("bad");
+
+if (wordBad > wordNot && wordNot !== -1 && wordBad !== -1) {
+  sentence = sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad + 3);
+}
+
+console.log(sentence);
+
+let sentence2 = "Fennel is bad, how could you not hate it"
+let wordNot2 = sentence2.indexOf("not");
+let wordBad2 = sentence2.indexOf("bad");
+
+if (wordBad2 > wordNot2 && wordNot2 !== -1 && wordBad2 !== -1) {
+  sentence2 = sentence2.slice(0, wordNot2) + "good" + sentence2.slice(wordBad2 + 3);
+}
+
+console.log(sentence2);
