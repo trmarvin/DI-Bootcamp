2e6ddbf6039c8d4e137e9184c741a60bc6429f48
Tamar Ron Marvin <trmarvin@gmail.com>
2025-09-25 13:01:20 +0300

    Week 10 Daily Challenge

---
+++ Week10/Day1/DailyChallenge/anagram.js
@@ -0,0 +1,32 @@
+/* Create a function that:
+
+takes in two strings as two parameters
+and returns a boolean that indicates whether or not the first string is an anagram of the second string.
+
+Do we need to consider whitespace?
+Trim whitespace prior to comparison. */
+
+function stringPrep(str) {
+  return str
+    .toLowerCase()   
+    .split(" ")
+    .join("");     
+}
+
+function areAnagrams(a, b) {
+  const A = stringPrep(a);
+  const B = stringPrep(b);
+  if (A.length !== B.length) return false;
+
+  // Sort approach
+  return [...A].sort().join("") === [...B].sort().join("");
+}
+
+const firstString = prompt("Enter the first string:");
+const secondString = prompt("Enter the second string:");
+
+if (areAnagrams(firstString, secondString)) {
+  alert("They are anagrams!");
+} else {
+  alert("These rae not anagrams.");
+}
