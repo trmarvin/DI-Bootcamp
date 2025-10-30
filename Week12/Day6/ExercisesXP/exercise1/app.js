6035c622b725705b4ced66df3975dc4fd4cac4fc
Tamar Ron Marvin <trmarvin@gmail.com>
2025-10-30 20:53:26 +0200

    W12D6XP1

---
+++ Week12/Day6/ExercisesXP/exercise1/app.js
@@ -0,0 +1,18 @@
+import express from "express";
+import router from "./routes/index.js";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+app.use("/", router);
+
+// Simple route
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
