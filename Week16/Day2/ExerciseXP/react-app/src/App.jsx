2f0ebc9ab14edc7f28af3623fcbcdb2f455c5fae
Tamar Ron Marvin <trmarvin@gmail.com>
2025-11-10 17:20:47 +0200

    W16D2XP1-2

---
+++ Week16/Day2/ExerciseXP/react-app/src/App.jsx
@@ -0,0 +1,35 @@
+import UserFavoriteAnimals from "./UserFavoriteAnimals";
+
+function App() {
+  const myelement = <h1>I Love JSX!</h1>;
+  const sum = 5 + 5;
+  const user = {
+    firstName: 'Bob',
+    lastName: 'Dylan',
+    favAnimals : ['Horse','Turtle','Elephant','Monkey']
+  };
+
+  return (
+    <div>
+      <div>
+        <p>Hello World!</p>
+      </div>
+      <div>
+        {myelement}
+      </div>
+      <div>
+        <p>React is {sum} times better with JSX.</p>
+      </div>
+      <div>
+        <h3>{user.firstName}</h3>
+        <h3>{user.lastName}</h3>
+      </div>
+      <div>
+        <h1>Favorite Animals</h1>
+        <UserFavoriteAnimals favAnimals={user.favAnimals} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
