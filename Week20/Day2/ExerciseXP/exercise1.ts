e85ef14f489715548c17e5faca58e184981d1536
Tamar Ron Marvin <trmarvin@gmail.com>
2025-12-22 15:08:30 +0200

    W20D2XP1&2

---
+++ Week20/Day2/ExerciseXP/exercise1.ts
@@ -0,0 +1,4 @@
+// Create a TypeScript program that logs the message “Hello, World!” to the console.
+
+const consoleMessage: string = "Hello, World!";
+console.log(consoleMessage);
---
+++ Week20/Day2/ExerciseXP/exercise2.ts
@@ -0,0 +1,10 @@
+// Define a variable age of type number and a variable name of type string, 
+// and log them to the console.
+
+let age: number;
+let personName: string;
+
+age = 18;
+personName = "Nathan";
+
+console.log(`${age}, ${personName}`)
